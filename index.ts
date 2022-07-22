2497e9db021c3a9f0693f33984accaaf6d51aa74
Belen <belenjarabanavas31@gmail.com>
2022-07-22 13:00:09 +0200

    return a html

--- index.ts
+++ index.ts
@@ -1,6 +1,38 @@
 export default {
-    port: 3000,
-    fetch(request: Request) {
-        return new Response("Hello world")
+  port: 3000,
+  fetch(request: Request) {
+    const { url, method } = request;
+
+    const { pathname } = new URL(url);
+
+    if (pathname === "/" && method === "GET") {
+      return new Response(`<h1>Hola mundo</h1>`, {
+        status: 200,
+        headers: {
+          "Content-Type": "text/html",
+        },
+      });
     }
-}
+
+    if (pathname === "/users" && method === "GET") {
+      return new Response(
+        JSON.stringify([
+          { name: "belen" },
+          { name: "lola" },
+          { name: "noelia" },
+          { name: "monica" },
+        ]),
+        {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    return new Response("", {
+      status: 404,
+    });
+  },
+};
